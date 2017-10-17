8271d75853ea9d481ac8ae337027443e448957c6
Ilyas Khurshid <ilyas.khurshid31@gmail.com>
2017-10-17 10:31:54 -0500

    Updated Layout. Can swipe Right and Left now. Swiping reveals buttons. Added a DeviceModel Java Class. Set up all menus.

--- app/src/main/java/com/example/averygrimes/phone_wallet_keys/MainActivity.java
+++ app/src/main/java/com/example/averygrimes/phone_wallet_keys/MainActivity.java
@@ -5,33 +5,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.baoyz.swipemenulistview.SwipeMenu;
-import com.baoyz.swipemenulistview.SwipeMenuCreator;
-import com.baoyz.swipemenulistview.SwipeMenuItem;
-import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import com.daimajia.swipe.util.Attributes;
+import com.example.averygrimes.phone_wallet_keys.DeviceModel;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-
-    private static final String TAG = "test";  //Show on the android monitor
     private BluetoothAdapter bluetooth;
     private Button addDevice, bluetoothSwitch; // Button references
     private int deviceTotal = 0;
 
-    private SwipeMenuListView listView;
-    private ArrayList<String> list;
-    private ArrayAdapter<String> adapter;
+    private TextView tvEmptyTextView;
+    private RecyclerView mRecyclerView;
+    private ArrayList<DeviceModel> mDataSet;
+    SwipeRecyclerViewAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,20 +40,97 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         bluetooth = BluetoothAdapter.getDefaultAdapter();
-        deviceTotal = 0;
-
-        listView = (SwipeMenuListView) findViewById(R.id.listView);
-
-        list = new ArrayList<>();
-        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        deviceTotal = 1;
 
         // Point references to the actual button
         addDevice = (Button) findViewById(R.id.addDevice);
         bluetoothSwitch = (Button) findViewById(R.id.bluetoothSwitch);
+        tvEmptyTextView = (TextView) findViewById(R.id.empty_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDrawable(R.drawable.divider)));
+        
+        mDataSet = new ArrayList<>();
+        
+        loadData();
+        
+        if(mDataSet.isEmpty()){
+            mRecyclerView.setVisibility(View.GONE);
+            tvEmptyTextView.setVisibility(View.VISIBLE);
+        }else{
+            mRecyclerView.setVisibility(View.VISIBLE);
+            tvEmptyTextView.setVisibility(View.GONE);
+        }
+        
+        
+        //creating adapter object
+        mAdapter = new SwipeRecyclerViewAdapter(this, mDataSet);
+        
+        
+        // Setting Mode to Single to reveal bottom View for one item in List
+        // Setting Mode to Mutliple to reveal bottom Views for multile items in List
+        ((SwipeRecyclerViewAdapter) mAdapter).setMode(Attributes.Mode.Single);
+        
+        mRecyclerView.setAdapter(mAdapter);
+        
+        /**Scroll listener**/
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState)
+            {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.e("RecyclerView", "onScrollStateChanged");
+            }
+            
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+            {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
 
         // If buttons are clicked, go to onclick method
         addDevice.setOnClickListener(this);
         bluetoothSwitch.setOnClickListener(this);
+    }
+    
+    // load initial data
+    public void loadData()
+    {
+        
+        for (int i = 1; i < 11; i++)
+        {
+            mDataSet.add(new DeviceModel("Device " + i, "Status"));
+            deviceTotal++;
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu dot){
+        getMenuInflater().inflate(R.menu.main, dot);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id =item.getItemId();
+        
+        if (id==R.id.id_setting){
+            Intent intentsetting = new Intent(MainActivity.this,Setting.class);
+            startActivity(intentsetting);
+            return true;
+        }
+        if (id==R.id.id_help){
+            return true;
+        }
+        if (id==R.id.id_history){
+            Intent intenthistory = new Intent(MainActivity.this,History.class);
+            startActivity(intenthistory);
+            return true;
+        }
+        return true;
     }
 
     @Override
@@ -63,63 +140,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             case R.id.addDevice:
             {
-                listView.setAdapter(adapter);
-                list.add("Device " + deviceTotal);
-
-                adapter.notifyDataSetChanged();
-
-                SwipeMenuCreator creator = new SwipeMenuCreator()
-                {
-                    @Override
-                    public void create(SwipeMenu menu) {
-                        // create "open" item
-                        SwipeMenuItem snoozeItem = new SwipeMenuItem(
-                                getApplicationContext());
-                        // set item background
-                        snoozeItem.setBackground(new ColorDrawable(Color.rgb(0x00, 0x66,
-                                0xff)));
-                        // set item width
-                        snoozeItem.setWidth(200);
-                        // set item title
-                        snoozeItem.setIcon(R.drawable.icon_snooze);
-
-                        snoozeItem.setId(0);
-                        // add to menu
-                        menu.addMenuItem(snoozeItem);
-
-                        // create "delete" item
-                        SwipeMenuItem settingsItem = new SwipeMenuItem(
-                                getApplicationContext());
-                        // set item background
-                        settingsItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
-                                0x3F, 0x25)));
-                        // set item width
-                        settingsItem.setWidth(200);
-                        // set a icon
-                        settingsItem.setIcon(R.drawable.icon_settings);
-                        // add to menu
-                        menu.addMenuItem(settingsItem);
-                    }
-                };
-
-                listView.setMenuCreator(creator);
-
-                listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
-                        switch (index) {
-                            case 0: // Snooze
-                                Log.d(TAG, "onMenuItemClick: clicked item " + index);
-
-                                break;
-                            case 1: // Settings
-                                Log.d(TAG, "onMenuItemClick: clicked item " + index);
-                                break;
-                        }
-                        // false : close the menu; true : not close the menu
-                        return false;
-                    }
-                });
+                mDataSet.add(new DeviceModel("Device " + deviceTotal, "Status"));
+                mRecyclerView.setAdapter(mAdapter);
 
                 deviceTotal++;
                 break;
@@ -129,46 +151,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 enableDisable();
                 break;
             }
+            case R.id.btn_Settings:
+            {
+
+
+                break;
+            }
         }
     }
 
-    //Create a BroadcastReceiver
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver()
-    {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            //when discovery finds a device
-            if (action.equals(bluetooth.ACTION_STATE_CHANGED)){
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, bluetooth.ERROR);
-                switch(state){
-                    case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "onReceive: STATE ON");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "mReceiver: STATE TURNING OFF");
-                        break;
-                    case BluetoothAdapter.STATE_OFF:
-                        Log.d(TAG, "onReceive: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "mReceiver: STATE TURNING OFF");
-                        break;
-                }
-            }
-        }
-    };
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        unregisterReceiver(mReceiver);
     }
 
     public void enableDisable()
     {
         if(bluetooth == null)
         {
-            Log.d(TAG, "bluetooth is not compatible with this device");
+            //Log.d(TAG, "bluetooth is not compatible with this device");
         }
 
         if(!bluetooth.isEnabled()){         //When Bluetooth is OFF will turn on bluetooth
@@ -178,7 +180,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             button.setText("OFF");
 
             IntentFilter BT = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mReceiver, BT);
         }
 
         if(bluetooth.isEnabled()){          //When Bluetooth is ON will turn off bluetooth
@@ -187,7 +188,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             button.setText("ON");
 
             IntentFilter BT = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mReceiver, BT);
         }
     }
 }
