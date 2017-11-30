a0150046dacee6162ad1c1b35f2cb61a6cb101f6
JanRaz <razojanet76@yahoo.com>
2017-11-30 15:49:19 -0600

    Add files via upload
    Place a database table on history where a entry is place on a table when someone adds a new device

---
+++ User.java
@@ -0,0 +1,31 @@
+package com.example.averygrimes.phone_wallet_keys;
+
+
+
+public class User {
+    private String bName;
+    private String bTime;
+    private String bDate;
+    //private String bStatus;
+
+    public User(String Dname, String Dtime, String Ddate){
+        bName = Dname;
+        bTime = Dtime;
+        bDate = Ddate;
+        //bStatus = Dstatus;
+    }
+
+    public String getbName(){
+        return bName;
+    }
+
+    public String getbTime(){
+        return bTime;
+    }
+
+    public String getbDate(){
+        return bDate;
+    }
+
+    //public String getbStatus(){ return bStatus; }
+}
