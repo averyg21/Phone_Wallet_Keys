f3fdb418b800a84997bc70ef100934bbe8dbf235
Ilyas Khurshid <ilyas.khurshid31@gmail.com>
2017-12-04 11:02:33 -0600

    update DeviceSettings.java
    Can now connect to devices

--- app/src/main/java/com/example/averygrimes/phone_wallet_keys/DeviceSettings.java
+++ app/src/main/java/com/example/averygrimes/phone_wallet_keys/DeviceSettings.java
@@ -1,28 +1,52 @@
 package com.example.averygrimes.phone_wallet_keys;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
 import android.app.AlertDialog;
 
 public class DeviceSettings extends AppCompatActivity implements View.OnClickListener
 {
+    private static final String TAG = "DeviceSettings";
     Button btn_DeviceSettings_OnOff, btn_DeviceSettings_Delete, btn_DeviceSettings_EditName, btn_DeviceSettings_Notification, btn_DeviceSettings_SnoozeTimer;
 
     BluetoothAdapter bluetoothAdapter;
-    private ArrayList<DeviceModel> connectedDeviceList;
-    Bundle extrasForDeviceSettings;
+    private ArrayList<BluetoothDevice> connectedDevice;
+    int connectedDevicesCounter;
+    String deviceAddress;
+
+    // Used to connect to the bluetooth device
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("0000110E-0000-1000-8000-00805F9B34FB");
+
+    Handler connectToDevice;
+
+    // Used for sending notification to phone
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,9 +54,16 @@ public class DeviceSettings extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_settings);
 
-        connectedDeviceList = new ArrayList<>();
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        extrasForDeviceSettings = getIntent().getExtras();
+        Bundle extrasForDeviceSettings = getIntent().getExtras();
+
+        connectedDevice = new ArrayList<>();
+        connectedDevicesCounter = extrasForDeviceSettings.getInt("connectedDevicesCounter");
+        connectedDevice = extrasForDeviceSettings.getParcelableArrayList("connectedDevice");
+        deviceAddress = extrasForDeviceSettings.getString("deviceAddress");
 
         btn_DeviceSettings_OnOff = (Button) findViewById(R.id.btn_DeviceSettings_OnOff);
         btn_DeviceSettings_Delete = (Button) findViewById(R.id.btn_DeviceSettings_Delete);
@@ -47,16 +78,99 @@ public class DeviceSettings extends AppCompatActivity implements View.OnClickLis
         btn_DeviceSettings_SnoozeTimer.setOnClickListener(this);
     }
 
+    //used to generate notification
+    public void createNotification()
+    {
+        //Build the notification
+        notification.setSmallIcon(R.drawable.oreo);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Lost Device Title");
+        notification.setContentText("Body of the notification");
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Build notification and issues it
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID, notification.build());
+    }
+
     @Override
     public void onClick(View view)
     {
-        String deviceAddress = extrasForDeviceSettings.getString("DeviceAddress");
         final BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(deviceAddress);
 
         switch (view.getId())
         {
             case R.id.btn_DeviceSettings_OnOff:
             {
+                try
+                {
+                    connectToDevice.removeCallbacksAndMessages(null);
+                }
+                catch (Exception ex)
+                {
+                    Log.d(TAG, "Post Delay Error: " + ex);
+                }
+
+                connectToDevice = new Handler();
+
+                connectToDevice.postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+
+                        if(bluetoothDevice.getBondState()==bluetoothDevice.BOND_BONDED)
+                        {
+                            Log.d(TAG, bluetoothDevice.getName());
+                            BluetoothSocket socket = null;
+
+                            try
+                            {
+                                socket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID_INSECURE);
+                            }
+                            catch (IOException e1)
+                            {
+                                // TODO Auto-generated catch block
+                                Log.d(TAG, "socket not created");
+                                e1.printStackTrace();
+                            }
+
+                            try
+                            {
+                                socket.connect();
+                                Log.e("","Connected");
+                                socket.close();
+                            }
+                            catch (IOException e)
+                            {
+                                Log.e("",e.getMessage());
+
+                                try
+                                {
+                                    Log.e("","trying fallback...");
+
+                                    socket =(BluetoothSocket) bluetoothDevice.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(bluetoothDevice,1);
+                                    socket.connect();
+
+                                    Log.e("","Connected");
+                                    socket.close();
+                                }
+                                catch (Exception e2)
+                                {
+                                    Log.e("", "Couldn't establish Bluetooth connection!");
+                                    createNotification();
+                                    connectToDevice.removeCallbacks(this);
+                                }
+                            }
+                        }
+                        connectToDevice.postDelayed(this, 5000);
+                    }
+                }, 5000);
 
                 break;
             }
@@ -164,7 +278,11 @@ public class DeviceSettings extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onBackPressed()
     {
-        Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(startIntent);
+        Context context = this.getApplicationContext();
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("connectedDevices", connectedDevice);
+        intent.putExtra("connectedDevicesCounter", connectedDevicesCounter);
+        context.startActivity(intent);
     }
 }
